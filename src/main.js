46de9c7d5ea1e4f0e95fe16196b7b2af042a3a63
brfinanceirooficial-hash <brfinanceirooficial@gmail.com>
2026-05-05 21:53:34 -0300

    style: substituição do ícone de igreja pela chama oficial da logo nas congregações

--- src/main.js
+++ src/main.js
@@ -571,7 +571,7 @@ function renderCongs() {
   grid.innerHTML = congregacoes.filter(c => c.destaque || congregacoes.indexOf(c) < 5).slice(0, 5).map(c => `
     <div class="cong-card reveal">
       <div class="cong-icon-wrap">
-        <i data-lucide="church"></i>
+        <svg class="cong-flame-icon" viewBox="0 0 48 46"><path d="M25.946 44.938c-.664.845-2.021.375-2.021-.698V33.937a2.26 2.26 0 0 0-2.262-2.262H10.287c-.92 0-1.456-1.04-.92-1.788l7.48-10.471c1.07-1.497 0-3.578-1.842-3.578H1.237c-.92 0-1.456-1.04-.92-1.788L10.013.474c.214-.297.556-.474.92-.474h28.894c.92 0 1.456 1.04.92 1.788l-7.48 10.471c-1.07 1.498 0 3.579 1.842 3.579h11.377c.943 0 1.473 1.088.89 1.83L25.947 44.94z"/></svg>
       </div>
       <h3>${c.nome}</h3>
       <p>${c.endereco}</p>
@@ -625,7 +625,7 @@ window.openModal = function(id) {
       </button>
       <div class="modal-header">
         <div class="modal-icon-wrap">
-          <i data-lucide="church"></i>
+          <svg class="cong-flame-icon" viewBox="0 0 48 46"><path d="M25.946 44.938c-.664.845-2.021.375-2.021-.698V33.937a2.26 2.26 0 0 0-2.262-2.262H10.287c-.92 0-1.456-1.04-.92-1.788l7.48-10.471c1.07-1.497 0-3.578-1.842-3.578H1.237c-.92 0-1.456-1.04-.92-1.788L10.013.474c.214-.297.556-.474.92-.474h28.894c.92 0 1.456 1.04.92 1.788l-7.48 10.471c-1.07 1.498 0 3.579 1.842 3.579h11.377c.943 0 1.473 1.088.89 1.83L25.947 44.94z"/></svg>
         </div>
         <h2>${c.nome}</h2>
         <p class="modal-endereco"><i data-lucide="map-pin"></i> ${c.endereco}</p>
